209424d68b6387ed62c36d0ca3d9869bc634b728
aliabbas90 <ali@abbas.net>
2026-03-17 17:11:10 +0100

    Add Google Tag Manager (GTM-TW29W5Z3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Tag Manager */}
+      <Script id="gtm" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TW29W5Z3');`}
+      </Script>
       {process.env.NEXT_PUBLIC_ADSENSE_ID && (
         <Script
           async
@@ -59,6 +67,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} font-sans antialiased bg-gray-50 min-h-screen flex flex-col`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TW29W5Z3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
